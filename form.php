95e1cd357ac025469c7f66998a3625b39ead2cad
unknown <2120367@talnet.nl>
2019-11-27 09:47:15 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="css/master.css">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="form" method="post">
+      <input type="text" name="" value="" placeholder="voer je e-mail adress in">
+      <input type="submit" name="submit" value="VERSTUUR">
+    </form>
+  </body>
+</html>
